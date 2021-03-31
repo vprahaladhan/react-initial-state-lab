df80fcb651843b1db7e52ca56ccd1149018e309f
prahal <vprahaladhan@gmail.com>
2021-03-31 13:44:12 +1100

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,18 @@
+import React, { Component } from 'react'; 
+
 // your Bomb code here!
+class Bomb extends Component {
+  constructor(props) {
+    super();
+    console.log('Initial count > ', props.initialCount);
+    this.state = {
+      secondsLeft: props.initialCount
+    };
+  }
+
+  render() {
+    return this.state.secondsLeft ? `${this.state.secondsLeft} seconds left before I go boom!` : 'Boom!';
+  }
+}
+
+export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,17 @@
+import React, { Component } from 'react'; 
+
 // your ImageSlider code here!
+class ImageSlider extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentSlideIndex: 0
+    };
+  }
+
+  render() {
+    return <div>I am on slide {this.state.currentSlideIndex}</div>
+  }
+}
+
+export default ImageSlider;
